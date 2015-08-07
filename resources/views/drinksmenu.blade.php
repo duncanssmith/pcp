7b4552d8eba5369912428d7a51ca26fcb4aefb95
Duncan Smith <duncanssmith@gmail.com>
2015-08-07 16:23:55 +0100

    Adding menus and PDF image files

---
+++ resources/views/drinksmenu.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Palm Court Pavilion</title>
+
+        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .top-photo {
+                background: #fefefe no-repeat top center;
+                min-height: 285px;
+                background-size: cover;
+                color: rgba(100,100,100,.9);
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .logo {
+                margin-top: 30px;
+                margin-bottom: 30px;
+            }
+
+            .title {
+                font-size: 64px;
+                color: #8a6;
+                font-weight: bold;
+            }
+
+            .address-box {
+
+            }
+
+            .address {
+                font-family: Lato ;
+                font-size: 24px;
+                line-height: 170%;
+            }
+
+        </style>
+    </head>
+    <body class="top-photo">
+      <div class="navigation">
+          <ul>
+              <li><a href="/mainmenu" class="btn btn-info" ><i class="fa fa-menu"></i>Main menu</a></li>
+              <li><a href="/drinksmenu" class="btn btn-info" ><i class="fa fa-menu"></i>Drinks</a></li>
+              <li><a href="/winelist" class="btn btn-info" ><i class="fa fa-menu"></i>Wine list</a></li>
+              <li><a href="/kidsmenu" class="btn btn-info" ><i class="fa fa-menu"></i>Kids menu</a></li>
+          </ul>
+      </div>
+        <div class="container">
+            <div class="content">
+                <div class="logo">
+                    <img src="/media/images/Home/logo.png" width="290px"/>
+                </div>
+
+                <br/>
+                <div class="title">Drinks</div>
+                <img src="/media/images/Menu/pcpDrinksMenu2015.pdf"/>
+
+            </div>
+        </div>
+    </body>
+</html>
