9fffe89417baeb67a5a04a0df042d7233e4e6076
Duncan Smith <duncanssmith@gmail.com>
2015-11-10 12:41:43 +0000

    HTML formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,70 +4,70 @@
         @include('layout.head')
     @show
     <body data-spy="scroll" data-target="#pcp-navbar" data-offset="90">
-      @section('nav')
-          @include('layout.nav')
-      @show
-      <div id="content">
-        <section id="section-welcome">
-        <h1 class="title">Welcome</h1>
-          <div class="container">
-              <div class="row">
-                  <div class="col-md-3">&nbsp;</div>
-                  <div class="col-md-6">
-                      <div class="logo">
-                          <img src="/media/images/Home/logo.png" width="290px"/>
-                      </div>
-                      <table class="main-table">
-                          <tr>
-                              <td colspan="1">
-                                  <a href="/menu" alt="Al fresco dining at Palm Court Pavilion" title="Dining outdoors at Palm Court Pavilion">
-                                      <img src="/media/images/Home/terrace.jpg" width="290px"/>
-                                  </a>
-                              </td>
-                              <td>
-                                  <a href="/menu" alt="Summer salads at Palm Court Pavilion" title="Delicious summer salads at Palm Court Pavilion">
-                                      <img src="/media/images/Home/salads.jpg" width="290px"/>
-                                  </a>
-                              </td>
-                              <td>
-                                  <a href="/menu" alt="Cream Teas at Palm Court Pavilion" title="Cream teas with jam and scones at Palm Court Pavilion">
-                                      <img src="/media/images/Home/teascones.jpg" width="290px"/>
-                                  </a>
-                              </td>
-                          </tr>
-                      </table>
-                  </div>
-                  <div class="col-md-3">&nbsp;</div>
-              </div>
-          </div>
-      </section>
+        @section('nav')
+            @include('layout.nav')
+        @show
+        <div id="content">
+            <section id="section-welcome">
+                <h1 class="title">Welcome</h1>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">&nbsp;</div>
+                        <div class="col-md-6">
+                            <div class="logo">
+                                <img src="/media/images/Home/logo.png" width="290px"/>
+                            </div>
+                            <table class="main-table">
+                                <tr>
+                                    <td colspan="1">
+                                        <a href="/menu" alt="Al fresco dining at Palm Court Pavilion" title="Dining outdoors at Palm Court Pavilion">
+                                            <img src="/media/images/Home/terrace.jpg" width="290px"/>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="/menu" alt="Summer salads at Palm Court Pavilion" title="Delicious summer salads at Palm Court Pavilion">
+                                            <img src="/media/images/Home/salads.jpg" width="290px"/>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="/menu" alt="Cream Teas at Palm Court Pavilion" title="Cream teas with jam and scones at Palm Court Pavilion">
+                                            <img src="/media/images/Home/teascones.jpg" width="290px"/>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-md-3">&nbsp;</div>
+                    </div>
+                </div>
+            </section>
 
-      <section id="section-about">
-      <h1 class="title">About</h1>
-            <div class="container">
-              <div class="row">
-                  <div class="col-md-3">&nbsp;</div>
-                  <div class="col-md-6">
-                          <p>
-                                Nestled in Worthing’s stunning Beach House Park, Palm Court Pavilion is a stylish,<br>
-                                beautifully renovated café-bistro in the park’s Old Pavilion. Located just minutes from<br>
-                                the town centre and the seafront, Palm Court Pavilion serves delicious homemade food<br>
-                                and has a range of options for functions and events.<br>
+            <section id="section-about">
+                <h1 class="title">About</h1>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">&nbsp;</div>
+                        <div class="col-md-6">
+                            <p>
+                                    Nestled in Worthing’s stunning Beach House Park, Palm Court Pavilion is a stylish,<br>
+                                    beautifully renovated café-bistro in the park’s Old Pavilion. Located just minutes from<br>
+                                    the town centre and the seafront, Palm Court Pavilion serves delicious homemade food<br>
+                                    and has a range of options for functions and events.<br>
                             </p>
                             <p>
-                                The cafe is an exciting new chapter in the Pavilion’s interesting history; after some years<br>
-                                in disuse, the Pavilion has been given a new lease of life. The landmark building has been<br>
-                                sympathetically renovated, emphasising the natural light and an extensive outdoor decking<br>
-                                area makes the most of the beautiful park setting. Both areas of decking can be used for<br>
-                                private parties.<br>
+                                    The cafe is an exciting new chapter in the Pavilion’s interesting history; after some years<br>
+                                    in disuse, the Pavilion has been given a new lease of life. The landmark building has been<br>
+                                    sympathetically renovated, emphasising the natural light and an extensive outdoor decking<br>
+                                    area makes the most of the beautiful park setting. Both areas of decking can be used for<br>
+                                    private parties.<br>
                             </p>
                             <p>
-                                The upstairs function room has a balcony giving delightful views of the park and bowling<br>
-                                greens, a perfect venue for parties, meetings and events.<br>
+                                    The upstairs function room has a balcony giving delightful views of the park and bowling<br>
+                                    greens, a perfect venue for parties, meetings and events.<br>
                             </p>
                             <p>
-                                The outside space in the park can also be used for larger scale events of up to 1000 people,<br>
-                                ideal for weddings, dining, exhibitions, corporate launches, markets and roadshows.<br>
+                                    The outside space in the park can also be used for larger scale events of up to 1000 people,<br>
+                                    ideal for weddings, dining, exhibitions, corporate launches, markets and roadshows.<br>
                             </p>
                         </div>
                         <div class="col-md-3"></div>
@@ -75,18 +75,18 @@
                 </div>
             </section>
             <section id="section-menu">
-            <h1 class="title">Menu</h1>
-              <div class="container">
-                <div class="row">
-                    <div class="col-md-2"></div>
+                <h1 class="title">Menu</h1>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-2"></div>
                         <div class="col-md-8">
                             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                                <!-- Indicators -->
+                            <!-- Indicators -->
                                 <ol class="carousel-indicators">
-                                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                                  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
                                 </ol>
 
                                 <!-- Wrapper for slides -->
@@ -131,22 +131,21 @@
                 </div>
             </section>
 
-        <section id="section-contact">
-        <h1 class="title">Contact</h1>
-          <div class="container">
-            <div class="address-box">
-                <p class="address">
-                Lyndhurst Road<br/>
-                Worthing<br/>
-                BN11 2DB<br/>
-                www.palmcourtpavilion.co.uk<br>
-                01903 212 011</p>
-            </div>
-
-          </div>
-
-        </section>
-
+            <section id="section-contact">
+                <h1 class="title">Contact</h1>
+                <div class="container">
+                    <div class="address-box">
+                        <p class="address">
+                            Lyndhurst Road<br/>
+                            Worthing<br/>
+                            BN11 2DB<br/>
+                            www.palmcourtpavilion.co.uk<br>
+                            01903 212 011
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </div>
     </body>
 </html>
 
