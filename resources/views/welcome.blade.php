82e30e58d1c5df6c8272b071e90b63cd2f890523
Duncan Smith <duncanssmith@gmail.com>
2016-01-15 21:18:57 +0000

    Updates with 2 new menus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,38 +11,12 @@
 
             <section id="section-welcome" class="section photo">
                 <div class="container">
-                    <div class="row">
+                    <div class="row top-row">
                         <div class="col-md-12">
                                 <h1 class="title pull-right">Palm Court Pavilion</h1>
                         </div>
                     </div>
-                </div>
-                <div class="spacer">
-                    <div class="row">
-                        <div class="col-md-12">
-                        </div>
-                    </div>
-                </div>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <br>
-                <div class="container" >
-                    <div class="row">
+                    <div class="row bottom-row">
                         <div class="col-md-12">
                             <p class="float-text-main">
                                 Palm Court Pavilion is a beautifully renovated, stylish café-bistro in Worthing's Beach House Park serving delicious homemade food in a tranquil park setting.  A matter of a few minutes walk from the town centre and the seafront, we also offer excellent facilities for events and functions.
@@ -54,12 +28,12 @@
 
             <section id="section-about" class="section flat">
                 <div class="container">
-                    <div class="row">
+                    <div class="row top-row">
                         <div class="col-md-12">
                             <h1 class="title pull-right">About us</h1>
                         </div>
                     </div>
-                    <div class="row photo-float">
+                    <div class="row middle-row">
                         <div class="col-md-5">
                                 <p class="address float-text">
                                     Palm Court Pavilion<br>
@@ -70,13 +44,13 @@
                                     www.palmcourtpavilion.co.uk<br>
                                     01903 212 011
                                 </p>
-                                <p class="address float-text"> Open daily 8am to 4:30pm </p>
+                                <p class="address float-text">Daily 8am to 4:30pm </p>
                         </div>
                         <div class="col-md-7">
                         </div>
                     </div>
 
-                    <div class="row">
+                    <div class="row bottom-row">
                         <div class="col-md-4">
                             <p class="float-text">
                                 Nestled in Worthing’s stunning Beach House Park, Palm Court Pavilion is a stylish, beautifully renovated café-bistro in the park’s Old Pavilion. Located just minutes from the town centre and the seafront, Palm Court Pavilion serves delicious homemade food and has a range of options for functions and events.
@@ -94,19 +68,19 @@
 
             <section id="section-functions" class="section flat">
                 <div class="container">
-                    <div class="row">
+                    <div class="row top-row">
                         <div class="col-md-12">
                             <h1 class="title pull-right">Functions</h1>
                         </div>
                     </div>
-                    <div class="row photo-float">
-                        <div class="col-md-6">
-                        </div>
-                        <div class="col-md-6">
-                        </div>
-                    </div>
+                    {{--<div class="row photo-float">--}}
+                        {{--<div class="col-md-6">--}}
+                        {{--</div>--}}
+                        {{--<div class="col-md-6">--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
 
-                    <div class="row">
+                    <div class="row bottom-row">
                         <div class="col-md-4">
                             <p class="float-text">
                                 The upstairs function room has a balcony giving delightful views of the park and bowling greens, a perfect venue for parties, meetings and events.
@@ -123,7 +97,7 @@
 
             <section id="section-views" class="section photo">
                 <div class="container">
-                    <div class="row">
+                    <div class="row top-row">
                         <div class="col-md-12">
                             <h1 class="title pull-right">Enjoy the views &hellip;</h1>
                         </div>
@@ -173,10 +147,12 @@
 
             <section id="section-menu" class="section photo">
                 <div class="container">
-                    <div class="row">
+                    <div class="row top-row">
                         <div class="col-md-12">
                             <h1 class="title pull-right">Our menu</h1>
                         </div>
+                    </div>
+                    <div class="row middle-row">
                         <div class="col-md-2">
 
                         </div>
@@ -188,11 +164,21 @@
                                     <li data-target="#carousel-menu" data-slide-to="1"></li>
                                     <li data-target="#carousel-menu" data-slide-to="2"></li>
                                     <li data-target="#carousel-menu" data-slide-to="3"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="4"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="5"></li>
                                 </ol>
 
                                 <!-- Wrapper for slides -->
                                 <div class="carousel-inner">
                                     <div class="item active">
+                                        <img src="/media/images/Menu/ValentinesMenu2016.png" />
+                                        <div class="carousel-caption"></div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/MothersDayMenu2016.png" />
+                                        <div class="carousel-caption"></div>
+                                    </div>
+                                    <div class="item">
                                         <img src="/media/images/Menu/MainMenu2015-2.png" />
                                         <div class="carousel-caption"></div>
                                     </div>
@@ -218,6 +204,9 @@
                         </div>
                         <div class="col-md-2">
                             <h4 class="title-sub pull-right">Download</h4>
+                            <a href="/media/images/Menu/ValentinesMenu2016.pdf" class="" target="_blank">Valentine's Day </a><br>
+                            <a href="/media/images/Menu/MothersDayMenu2016.pdf" class="" target="_blank">Mother's Day </a><br>
+                            <a href="/media/images/Menu/PalmCourtMainMenu2015.pdf" class="" target="_blank">Main menu </a><br>
                             <a href="/media/images/Menu/PalmCourtMainMenu2015.pdf" class="" target="_blank">Main menu </a><br>
                             <a href="/media/images/Menu/PalmCourtDrinksMenu2015.pdf" class="" target="_blank">Drinks </a><br>
                             <a href="/media/images/Menu/PalmCourtWineList2015.pdf" class="" target="_blank">Wine list </a><br>
