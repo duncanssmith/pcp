e202fbdd103e4cac785f482adf448d3d6d0fdd73
Duncan Smith <duncanssmith@gmail.com>
2016-01-16 23:50:44 +0000

    Groupon and small improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,16 +16,33 @@
                                 <h1 class="title pull-right">Palm Court Pavilion</h1>
                         </div>
                     </div>
-                    <div class="row bottom-row">
+                    <div class="row middle-row">
                         <div class="col-md-12">
                             <p class="float-text-main">
-                                Palm Court Pavilion is a beautifully renovated, stylish café-bistro in Worthing's Beach House Park serving delicious homemade food in a tranquil park setting.  A matter of a few minutes walk from the town centre and the seafront, we also offer excellent facilities for events and functions.
+                                Palm Court Pavilion is a beautifully renovated, stylish café-bistro in Worthing's delightful Beach House Park.
+                                We serve delicious homemade food in a tranquil setting, and are a few minutes walk from the town centre and the seafront. We also offer excellent facilities for events and functions.
                             </p>
+                        </div>
+                    </div>
+                    <div class="row bottom-row">
+                        <div class="col-md-6">
+                            <div class="float-text-main">
+                                <img src="/media/images/Home/logo-transparent.png" width="140px"/>
+                                <h1><a href="https://www.groupon.co.uk/deals/palm-court-pavilion-1">Our <span class="groupon-logo-style">GROUPON</span> offer</a></h1>
+                                <p>
+                                    Enjoy sandwiches, scones with cream and jam, and a slice of cake served with a pot of tea and a glass of Prosecco.
+                                </p>
+                                <h2> Choose Between Two Options </h2>
+                                <p>
+                                    £15 for an afternoon tea for two with a glass of Prosecco (40% off)<br>
+                                    £29 for an afternoon tea for four with a glass of Prosecco (42% off)
+                                </p>
+                            </div>
+                            <div class="col-md-6"></div>
                         </div>
                     </div>
                 </div>
             </section>
-
             <section id="section-about" class="section flat">
                 <div class="container">
                     <div class="row top-row">
@@ -73,12 +90,6 @@
                             <h1 class="title pull-right">Functions</h1>
                         </div>
                     </div>
-                    {{--<div class="row photo-float">--}}
-                        {{--<div class="col-md-6">--}}
-                        {{--</div>--}}
-                        {{--<div class="col-md-6">--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
 
                     <div class="row bottom-row">
                         <div class="col-md-4">
@@ -171,25 +182,23 @@
                                 <!-- Wrapper for slides -->
                                 <div class="carousel-inner">
                                     <div class="item active">
-                                        <img src="/media/images/Menu/ValentinesMenu2016.png" />
+                                        <img src="/media/images/Menu/ValentinesMenu2016.jpg" />
                                         <div class="carousel-caption"></div>
                                     </div>
                                     <div class="item">
-                                        <img src="/media/images/Menu/MothersDayMenu2016.png" />
-                                        <div class="carousel-caption"></div>
+                                        <img src="/media/images/Menu/MothersDayMenu2016.jpg" />
                                     </div>
                                     <div class="item">
-                                        <img src="/media/images/Menu/MainMenu2015-2.png" />
-                                        <div class="carousel-caption"></div>
+                                        <img src="/media/images/Menu/MainMenu2015-3.png" />
                                     </div>
                                     <div class="item">
-                                        <img src="/media/images/Menu/DrinksMenu2015-2.png" />
+                                        <img src="/media/images/Menu/DrinksMenu2015-3.png" />
                                     </div>
                                     <div class="item">
-                                        <img src="/media/images/Menu/KidsMenu2015-2.png" />
+                                        <img src="/media/images/Menu/KidsMenu2015-3.png" />
                                     </div>
                                     <div class="item">
-                                        <img src="/media/images/Menu/WineList2015-2.png" />
+                                        <img src="/media/images/Menu/WineList2015-3.png" />
                                     </div>
                                 </div>
 
