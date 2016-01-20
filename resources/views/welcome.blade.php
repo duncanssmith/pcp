24b4bcfa081a2c89f1272f6aecc909a6e1feaf40
Duncan Smith <duncanssmith@gmail.com>
2016-01-20 10:10:23 +0000

    Remove groupon from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,23 +24,23 @@
                             </p>
                         </div>
                     </div>
-                    <div class="row bottom-row">
-                        <div class="col-md-6">
-                            <div class="float-text-main">
-                                <img src="/media/images/Home/logo-transparent.png" width="140px"/>
-                                <h1><a href="https://www.groupon.co.uk/deals/palm-court-pavilion-1">Our <span class="groupon-logo-style">GROUPON</span> offer</a></h1>
-                                <p>
-                                    Enjoy sandwiches, scones with cream and jam, and a slice of cake served with a pot of tea and a glass of Prosecco.
-                                </p>
-                                <h2> Choose Between Two Options </h2>
-                                <p>
-                                    £15 for an afternoon tea for two with a glass of Prosecco (40% off)<br>
-                                    £29 for an afternoon tea for four with a glass of Prosecco (42% off)
-                                </p>
-                            </div>
-                            <div class="col-md-6"></div>
-                        </div>
-                    </div>
+                    {{--<div class="row bottom-row">--}}
+                        {{--<div class="col-md-6">--}}
+                            {{--<div class="float-text-main">--}}
+                                {{--<img src="/media/images/Home/logo-transparent.png" width="140px"/>--}}
+                                {{--<h1><a href="https://www.groupon.co.uk/deals/palm-court-pavilion-1">Our <span class="groupon-logo-style">GROUPON</span> offer</a></h1>--}}
+                                {{--<p>--}}
+                                    {{--Enjoy sandwiches, scones with cream and jam, and a slice of cake served with a pot of tea and a glass of Prosecco.--}}
+                                {{--</p>--}}
+                                {{--<h2> Choose Between Two Options </h2>--}}
+                                {{--<p>--}}
+                                    {{--£15 for an afternoon tea for two with a glass of Prosecco (40% off)<br>--}}
+                                    {{--£29 for an afternoon tea for four with a glass of Prosecco (42% off)--}}
+                                {{--</p>--}}
+                            {{--</div>--}}
+                            {{--<div class="col-md-6"></div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
                 </div>
             </section>
             <section id="section-about" class="section flat">
