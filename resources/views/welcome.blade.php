a6fe9cf98d320883314b15ae18c0cbfae54048ff
Duncan Smith <duncanssmith@gmail.com>
2016-01-28 17:08:09 +0000

    Re-order pages and reinstate menu page from external page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,12 @@
         @section('nav')
             @include('layout.nav')
         @show
-        <div id="content">
+        <section id="content">
 
             <section id="section-welcome" class="section photo">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-6 col-md-offset-6">
                                 <h1 class="title pull-right">Palm Court Pavilion</h1>
                         </div>
                     </div>
@@ -43,64 +43,68 @@
                     {{--</div>--}}
                 </div>
             </section>
-            <section id="section-about" class="section flat">
+
+            <section id="section-menu" class="section photo">
                 <div class="container">
                     <div class="row top-row">
-                        <div class="col-md-12">
-                            <h1 class="title pull-right">About us</h1>
+                        <div class="col-md-6 col-md-offset-6">
+                            <h1 class="title pull-right">Our menu</h1>
                         </div>
                     </div>
                     <div class="row middle-row">
-                        <div class="col-md-5">
-                                <p class="address float-text">
-                                    Palm Court Pavilion<br>
-                                    Beach House Park<br>
-                                    Lyndhurst Road<br>
-                                    Worthing<br>
-                                    BN11 2DB<br>
-                                    www.palmcourtpavilion.co.uk<br>
-                                    01903 212 011
-                                </p>
-                                <p class="address float-text">Daily 8am to 4:30pm </p>
-                        </div>
-                        <div class="col-md-7">
-                            <p class="float-text">
-                                Nestled in Worthing’s stunning Beach House Park, Palm Court Pavilion is a stylish, beautifully renovated café-bistro in the park’s Old Pavilion. Located just minutes from the town centre and the seafront, Palm Court Pavilion serves delicious homemade food and has a range of options for functions and events.
-                            </p>
-                            <p class="float-text">
-                                The cafe marks an exciting new chapter in the Pavilion’s history. After some years in disuse, the Pavilion has been given a new lease of life. The landmark building has been sympathetically renovated to emphasise natural light inside, and large decking areas outside have been added so that visitors can enjoy the beautiful park setting. The decking areas can also be used for private parties or functions.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row bottom-row">
-                        <div class="col-md-12">
-                            <div class="google-map-container">
-                                <h1>How to find us</h1>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.9094489491195!2d-0.36574868437995894!3d50.81431636896051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDQ4JzUxLjUiTiAwwrAyMSc0OC44Ilc!5e0!3m2!1sen!2suk!4v1451944856246" width="100%" height="400px" frameborder="2" style="border:2px; padding:4px;" ></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
 
-            <section id="section-functions" class="section flat">
-                <div class="container">
-                    <div class="row top-row">
-                        <div class="col-md-12">
-                            <h1 class="title pull-right">Functions</h1>
-                        </div>
-                    </div>
+                        <div class="col-md-8 col-md-offset-2">
+                            <div id="carousel-menu" class="carousel slide" data-ride="carousel">
+                                <!-- Indicators -->
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-menu" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="1"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="2"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="3"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="4"></li>
+                                    <li data-target="#carousel-menu" data-slide-to="5"></li>
+                                </ol>
 
-                    <div class="row bottom-row">
-                        <div class="col-md-4">
-                            <p class="float-text">
-                                The upstairs function room has a balcony giving delightful views of the park and bowling greens, a perfect venue for parties, meetings and events.
-                            </p>
+                                <!-- Wrapper for slides -->
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img src="/media/images/Menu/png/ValentinesMenu2016-4.png" />
+                                        <div class="carousel-caption"></div>
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/png/MothersDayMenu2016-4.png" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/png/MainMenu2015-4.png" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/png/DrinksMenu2015-4.png" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/png/KidsMenu2015-4.png" />
+                                    </div>
+                                    <div class="item">
+                                        <img src="/media/images/Menu/png/WineList2015-4.png" />
+                                    </div>
+                                </div>
+
+                                <!-- Controls -->
+                                <a class="left carousel-control" href="#carousel-menu" role="button" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-menu" role="button" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div> <!-- Carousel -->
                         </div>
-                        <div class="col-md-4">
-                            <p class="float-text">
-                                The outside space in the park can be used for larger scale events of up to 1000 people, ideal for weddings, dining, exhibitions, corporate launches, markets and roadshows.<br>
-                            </p>
+                        <div class="col-md-2">
+                            <h4 class="title-sub pull-right">Download</h4>
+                            <a href="/media/images/Menu/pdf/ValentinesMenu2016.pdf" class="" target="_blank">Valentine's Day </a><br>
+                            <a href="/media/images/Menu/pdf/MothersDayMenu2016.pdf" class="" target="_blank">Mother's Day </a><br>
+                            <a href="/media/images/Menu/pdf/PalmCourtMainMenu2015.pdf" class="" target="_blank">Main menu </a><br>
+                            <a href="/media/images/Menu/pdf/PalmCourtDrinksMenu2015.pdf" class="" target="_blank">Drinks </a><br>
+                            <a href="/media/images/Menu/pdf/PalmCourtKidsMenu2015.pdf" class="" target="_blank">Kid's menu </a><br>
+                            <a href="/media/images/Menu/pdf/PalmCourtWineList2015.pdf" class="" target="_blank">Wine list </a><br>
                         </div>
                     </div>
                 </div>
@@ -109,7 +113,7 @@
             <section id="section-views" class="section photo">
                 <div class="container">
                     <div class="row top-row">
-                        <div class="col-md-12">
+                        <div class="col-md-6 col-md-offset-6">
                             <h1 class="title pull-right">Enjoy the views &hellip;</h1>
                         </div>
                     </div>
@@ -137,6 +141,69 @@
                                 <img src="/media/images/Home/gTerrace.jpg" width="400px">
                             </div>
 
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="section-functions" class="section flat">
+                <div class="container">
+                    <div class="row top-row">
+                        <div class="col-md-6 col-md-offset-6">
+                            <h1 class="title pull-right">Functions</h1>
+                        </div>
+                    </div>
+
+                    <div class="row bottom-row">
+                        <div class="col-md-4">
+                            <p class="float-text">
+                                The upstairs function room has a balcony giving delightful views of the park and bowling greens, a perfect venue for parties, meetings and events.
+                            </p>
+                        </div>
+                        <div class="col-md-4">
+                            <p class="float-text">
+                                The outside space in the park can be used for larger scale events of up to 1000 people, ideal for weddings, dining, exhibitions, corporate launches, markets and roadshows.<br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="section-about" class="section flat">
+                <div class="container">
+                    <div class="row top-row">
+                        <div class="col-md-6 col-md-offset-6">
+                            <h1 class="title pull-right">About us</h1>
+                        </div>
+                    </div>
+                    <div class="row middle-row">
+                        <div class="col-md-5">
+                            <p class="address float-text">
+                                Palm Court Pavilion<br>
+                                Beach House Park<br>
+                                Lyndhurst Road<br>
+                                Worthing<br>
+                                BN11 2DB<br>
+                                www.palmcourtpavilion.co.uk<br>
+                                01903 212 011
+                            </p>
+                            <p class="address float-text">Daily 8am to 4:30pm </p>
+                        </div>
+                        <div class="col-md-7">
+                            <p class="float-text">
+                                Nestled in Worthing’s stunning Beach House Park, Palm Court Pavilion is a stylish, beautifully renovated café-bistro in the park’s Old Pavilion. Located just minutes from the town centre and the seafront, Palm Court Pavilion serves delicious homemade food and has a range of options for functions and events.
+                            </p>
+                            <p class="float-text">
+                                The cafe marks an exciting new chapter in the Pavilion’s history. After some years in disuse, the Pavilion has been given a new lease of life. The landmark building has been sympathetically renovated to emphasise natural light inside, and large decking areas outside have been added so that visitors can enjoy the beautiful park setting. The decking areas can also be used for private parties or functions.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row bottom-row">
+                        <div class="col-md-12">
+                            <div class="google-map-container">
+                                <h1>How to find us</h1>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.9094489491195!2d-0.36574868437995894!3d50.81431636896051!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTDCsDQ4JzUxLjUiTiAwwrAyMSc0OC44Ilc!5e0!3m2!1sen!2suk!4v1451944856246" width="100%" height="400px" frameborder="2" style="border:2px; padding:4px;" ></iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
