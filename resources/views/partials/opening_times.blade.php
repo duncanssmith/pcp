371e643dd941e66e5c39a21499dd83108fe71228
Duncan Smith <duncanssmith@gmail.com>
2017-01-09 12:50:27 +0000

    Opening times for january 2017 15 minutes

--- resources/views/partials/opening_times.blade.php
+++ resources/views/partials/opening_times.blade.php
@@ -1,2 +1,5 @@
-Monday to Sunday: 8am to 5pm<br>
-Functions, private hire: by arrangement
+January opening times<br>
+Monday: closed<br>
+Tuesday to Friday: 9am to 4pm<br>
+Saturday and Sunday: 8am to 5pm<br>
+Evenings and private functions available by arrangement
