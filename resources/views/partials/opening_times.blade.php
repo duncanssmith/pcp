0f7eb4e244fb7780db0dc43eb9b93fa63e8f39b5
Duncan Smith <duncanssmith@gmail.com>
2017-05-02 14:33:41 +0100

    Update opening times and Functions document 20 mins

--- resources/views/partials/opening_times.blade.php
+++ resources/views/partials/opening_times.blade.php
@@ -1,11 +1,13 @@
                         <div class="col-md-4">
-                            <h4 class="opening-times float-text">January opening times:</h4>
+                            <h4 class="opening-times float-text">Opening times:</h4>
                         </div>
                         <div class="col-md-8">
                             <p class="opening-times float-text">
-Monday: 12 noon to 5pm<br>
-Tuesday to Friday: 9am to 4pm<br>
-Saturday and Sunday: 8am to 5pm<br>
-Evenings and private functions available by arrangement
+                              Monday: Midday to 4pm<br>
+                              Bank Holiday Mondays: 9am to 6pm<br>
+                              Tuesday to Friday: 9am to 6pm<br>
+                              Saturday and Sunday: 8am to 6pm<br>
+                              Evenings and private functions available by arrangement<br>
+                              We may close earlier that stated depending on the weather
                             </p>
                         </div>
