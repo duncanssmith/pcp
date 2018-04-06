289ee7108978abb8f702ebee81d8e2b282e79570
Duncan Smith <duncanssmith@gmail.com>
2018-04-06 16:26:56 +0100

    New menus and change to opening times - 30 minutes

--- resources/views/partials/opening_times.blade.php
+++ resources/views/partials/opening_times.blade.php
@@ -1,8 +1,10 @@
 <h4 class="opening-times float-text">Opening times</h4>
 <p class="opening-times float-text">
-  Monday: Closed<br>
-  Tuesday to Friday: 9am to 4pm<br>
+  Monday: Closed (available for private functions)<br>
+  Tuesday to Friday: 9am to 5pm<br>
   Saturday and Sunday: 8am to 5pm<br>
+  We may close earlier than stated depending on the weather<br/>
+  Please call ahead to avoid any disappointment!<br/>
   Evenings and private functions available by arrangement.<br>
   Phone: 01903 212 011
 </p>
