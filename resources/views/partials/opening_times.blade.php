f2f407c0cb98731b8ff5f33993818dab3dff93b7
Duncan Smith <duncanssmith@gmail.com>
2018-06-12 16:23:28 +0100

    Minor changes to opening times text - 15 mins

--- resources/views/partials/opening_times.blade.php
+++ resources/views/partials/opening_times.blade.php
@@ -3,11 +3,11 @@
   Monday: Closed (available for private functions)<br/>
   Bank Holidays: Open 9am to 5pm<br/>
   Tuesday to Friday: 9am to 5pm<br/>
-  Saturday and Sunday: 8am to 5pm (last orders 4pm)<br/>
+  Saturday and Sunday: 8am to 5pm (last orders 4pm)
 </p>
 <p class="opening-times float-text">
   <u>Please note</u><br/> 
-  <b>Saturday 19th May: Last orders 3:30pm due to private function</b><br/>
+  {{-- <b>Saturday 19th May: Last orders 3:30pm due to private function</b><br/> --}}
   We may close earlier than stated depending on the weather<br/>
   Please call ahead to avoid any disappointment!<br/>
   Evenings and private functions available by arrangement.<br/>
